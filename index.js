1f436558393a01a9c7245e16352ae9a8fb2babd9
Jesse <eoshout@Gmail.com>
2021-09-02 16:49:50 -0400

    mvp

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const server = require('express')()
+require("dotenv").config();
 
-server.listen(5000, () => {
-    console.log('listening on port 5000')
-})
+const express = require("express");
+const cors = require("cors");
+
+const DaddyPORT = process.env.PORT || 5000;
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.get("/", (req, res) => {
+  res.send(`<h1>HEROKU  APP</h1>`);
+});
+
+server.get("/api", (req, res) => {
+  res.json({ message: "HEROKU  APP" });
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`listening on port ${DaddyPORT}`);
+});
